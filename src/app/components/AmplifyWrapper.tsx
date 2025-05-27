a7b4d7509534d266b05c8e358f7db60fbe1c55be
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2025-05-27 15:45:14 -0600

    feat: Add styles from Amplify IU, importing @aws-amplify/ui-react/styles.css at layout and grant context across amplify wrapper component, and use it in page.tsx

---
+++ src/app/components/AmplifyWrapper.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { ThemeProvider } from '@aws-amplify/ui-react';
+
+export function AmplifyWrapper({ children }: { children: React.ReactNode }) {
+  return <ThemeProvider>{children}</ThemeProvider>;
+}
