a3e6ad5ae0d2dd3d16b77e3996bbba66bf31789d
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2025-05-28 21:30:07 -0600

    feat(data): Add data examples to build the new components

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,6 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { Certificate } from 'crypto';
+import { act } from 'react';
 
 const schema = a.schema({
   Certifications: a
@@ -21,9 +23,12 @@ const schema = a.schema({
   Projects: a
     .model({
       title: a.string().required(),
+      photoUrl: a.url(),
       description: a.string().required(),
+      place: a.string().required(),
       projectUrl: a.url(),
       skills: a.string().array(),
+      gallery: a.string().array(),
       categories: a.enum(['Hackathon', 'Research', 'Professional', 'Academic', 'Personal']),
     })
     .authorization((allow) => [
@@ -45,6 +50,39 @@ const schema = a.schema({
       allow.guest().to(['read']),
       allow.owner()
     ]),
+
+    Education: a
+    .model({
+      institution: a.string().required(),
+      degree: a.string().required(),
+      fieldOfStudy: a.string(),
+      startDate: a.date().required(),
+      endDate: a.date(),
+      description: a.string(),
+      location: a.string(),
+      recognition: a.string().array(),
+      CertificateURL: a.url(),
+      Photos: a.string().array(),
+    }), 
+
+    Languages: a
+    .model({
+      language: a.string().required(),
+      proficiency: a.enum(['Basic', 'Conversational', 'Fluent', 'Native']),
+    }), 
+
+    Experiences: a
+    .model({
+      company: a.string().required(),
+      position: a.string().required(),
+      startDate: a.date().required(),
+      endDate: a.date(),
+      description: a.string(),
+      location: a.string(),
+      skills: a.string().array(),
+      activities: a.string().array(),
+    }),
+
 
 
 });
